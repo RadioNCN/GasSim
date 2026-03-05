eae4219593b2cbf3438f1bdd5c6cee81ca9dc281
N.Nickig <niklas.nickig@outlook.de>
2026-03-05 13:15:35 +0100

    Impl Norm

---
+++ src/Helper/norm.rs
@@ -0,0 +1,10 @@
+use std::ops::{Add, Sub, Div, Mul};
+pub fn norm_FN<I, Y, O>(x: &I, min_i:&I, max_i:&I, min_o:&O, max_o:&O) -> O
+where
+    I: Copy + Add<Output = I> + Sub<Output = I> + Div<Output = Y> + Div<f64, Output = I>,
+    O: Copy + Add<Output = O> + Sub<Output = O> + Div<f64, Output = O>,
+    Y: Copy + Mul<O, Output = O>
+{
+    let y = (*x - (*max_i + *min_i) / 2.) / (*max_i - *min_i);
+    return y * (*max_o - *min_o) + (*max_o + *min_o) / 2.;
+}
